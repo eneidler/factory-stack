6e8adc182a2f6ab8c323018758ad65c3326104da
Evan Neidler <evan.neidler@glitchd.net>
2019-08-30 15:56:31 -0400

    Fixed bug where subproperties of the Job class were not displaying through dot notation when binding. The issue was resolved by using .Include() in the using statements/data context. See eager loading vs. lazy loading.

--- ProductionScheduler/Services/JobManager.cs
+++ ProductionScheduler/Services/JobManager.cs
@@ -3,13 +3,14 @@ using ProductionScheduler.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProductionScheduler.Services
 {
-    public sealed class JobManager
+    public sealed class JobManager : BaseViewModel
     {
 
         private static readonly object _padlock = new object();
@@ -42,12 +43,17 @@ namespace ProductionScheduler.Services
             {
                 using(ProductionSchedulerContext context = new ProductionSchedulerContext())
                 {
-                    var jobList = context.Jobs.Where(j => j.IsComplete == false);
+                    var jobList = context.Jobs.Where(j => j.IsComplete == false).Include(b => b.Part).Include(b => b.Mold).Include(b => b.Press);
 
                     _activeJobList = jobList.ToList();
                 }
 
                 return _activeJobList;
+            }
+            set
+            {
+                _activeJobList = value;
+                NotifyOnPropertyChanged(nameof(ActiveJobList));
             }
         }
     }
